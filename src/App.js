8720b8292c833a7badbbfe2be6fd8accd0b8b986
wayne chan <waynestock666@gmail.com>
2024-05-20 23:58:56 +0100

    add clarify AI

--- src/App.js
+++ src/App.js
@@ -4,25 +4,100 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import ParticlesBg from 'particles-bg'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ParticlesBg from 'particles-bg';
 
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const PAT = '0560c6cc8b6c4d42ad22a03d5117f2e1';
+  const USER_ID = 'waynexchan';
+  const APP_ID = 'test';
+  const IMAGE_URL = imageUrl;
 
-class App extends Component{
-  render(){
-    return(
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                    // "base64": IMAGE_BYTES_STRING
+                }
+            }
+        }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+    this.MODEL_ID = 'color-recognition'; // 定義 MODEL_ID
+    this.MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105'; // 定義 MODEL_VERSION_ID
+  }
+
+  calculateFaceLocation = (data) => {
+    if (data && data.outputs && data.outputs[0].data.regions) {
+      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+    }
+    return {};
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    fetch(`https://api.clarifai.com/v2/models/${this.MODEL_ID}/versions/${this.MODEL_VERSION_ID}/outputs`, returnClarifaiRequestOptions(this.state.input))
+      .then(response => response.json())
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
       <div className="App">
         <ParticlesBg type="random" bg={true} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*
-          
-          <FaceRecognition />*/}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
 }
-
 
 export default App;
